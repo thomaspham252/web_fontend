18e2392ab1e4a4dae9fac9007d1c8713ae79839e
thomaspham252 <22130143@st.hcmuaf.edu.vn>
2026-01-02 21:09:18 +0700

    Hoàn thành trang sản phẩm fixes #7

--- soap_website/src/App.jsx
+++ soap_website/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/header_footer/Header";
 import Footer from "./components/header_footer/Footer";
-
+import Products from "./pages/Products";
 function App() {
     return (
         <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
 
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
             </Routes>
 
             <Footer />
