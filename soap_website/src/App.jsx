4452724c9566c50bd46f73009c4a294d33cb9427
Quoc Toan <22130293@st.hcmuaf.edu.vn>
2026-01-05 19:06:02 +0700

    fix app

--- soap_website/src/App.jsx
+++ soap_website/src/App.jsx
@@ -9,18 +9,26 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import User from "./pages/User";
+
 function App() {
     return (
         <BrowserRouter>
             <Header />
 
             <Routes>
+                {/* Trang chủ */}
                 <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} /> {/* Thêm dòng này để vào tên miền chính là ra Home luôn */}
+
                 <Route path="/products" element={<Products />} />
                 <Route path="/products/:id" element={<ProductDetail />} />
                 <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
                 <Route path="/about" element={<About />} />
 
+                {/* --- BẠN BỊ THIẾU ĐOẠN NÀY --- */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/user" element={<User />} />
 
             </Routes>
 
